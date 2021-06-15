c8817c7d8cc848a7c373e2e9635b83591f48c91c
Yusuf ARSLAN <arslanon@outlook.com>
2021-06-15 19:34:26 +0300

    3ve4 tam bölünenler toplamı

--- patikaJava101/src/donguler/ciftSayi.java
+++ patikaJava101/src/donguler/ciftSayi.java
@@ -1,10 +1,29 @@
 package donguler;
 
+import java.util.Scanner;
+
 public class ciftSayi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Kodluyoruz patika.dev Java patikasý Döngüler pratik 1
+		// Girilen Sayýya Kadar Olan Çift Sayýlarý Bulan Program
+		// Java döngüler ile 0'dan girilen sayýya kadar olan sayýlardan 3 ve 4'e tam bölünen sayýlarýn ortalamasýný hesaplayan programý yazýnýz.
+		
+		int sayi,adet=0;
+		int toplam=0;
 
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Sayý giriniz: ");
+		sayi = input.nextInt();
+
+		for (int i = 0; i <= sayi; i++) {
+			if (i%12==0) {//3 ve 4 e tam bölünen her sayý 12 e tam bölünür
+				adet++;
+				toplam+=i;				
+			}
+		}
+		System.out.println("Ortalama= "+ (toplam/adet));
 	}
 
 }
