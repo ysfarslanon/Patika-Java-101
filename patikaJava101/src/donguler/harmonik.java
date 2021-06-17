93d80bf03ca91e7358f172dc47e410059cd53b90
Yusuf ARSLAN <arslanon@outlook.com>
2021-06-18 01:31:28 +0300

    harmonik

---
+++ patikaJava101/src/donguler/harmonik.java
@@ -0,0 +1,28 @@
+package donguler;
+
+import java.util.Scanner;
+
+public class harmonik {
+
+	public static void main(String[] args) {
+		// Kodluyoruz patika.dev Java patikasý Döngüler pratik 6
+		// Armstrong Sayýlarý Bulan Program
+		// BJava ile girilen sayýnýn harmonik serisini bulan program yazacaðýz.
+		
+		double n,result=0;
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Sayý giriniz: ");
+		n=input.nextDouble();
+		
+		while (n>0) {
+			result+=(1/n);
+			n--;
+		}
+		
+		System.out.print(result);
+
+	}
+
+}
