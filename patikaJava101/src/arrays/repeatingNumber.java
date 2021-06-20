4f70f02eba7bb8cce01934f3524a776ff7dac322
Yusuf ARSLAN <arslanon@outlook.com>
2021-06-21 01:43:22 +0300

    repeatingNumber

---
+++ patikaJava101/src/arrays/repeatingNumber.java
@@ -0,0 +1,39 @@
+package arrays;
+
+public class repeatingNumber {
+	static boolean isFind(int[] arr,int value) {
+		for(int i:arr) {
+			if(i==value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// Kodluyoruz patika.dev Java patikasý diziler pratik 4
+		// Dizideki Tekrar Eden Sayýlarý Bulan Program
+
+		int[] list = { 3, 7, 3, 3, 2, 9, 10, 21, 1, 9, 33, 1 };
+
+		int[] dublicate=new int[list.length];
+		
+		int startIndex=0;
+		for (int i = 0; i < list.length; i++) {
+			for (int j = 0; j < list.length; j++) {
+				if (i!=j && list[i]==list[j]) {
+					if(!isFind(dublicate,list[i])) {
+						dublicate[startIndex++]=list[i];					
+					}
+					break;
+				}
+			}
+		}
+		for (int value:dublicate){
+			if(value!=0) {
+				System.out.println(value);
+			}
+		}
+	}
+
+}
